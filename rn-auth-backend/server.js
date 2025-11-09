f6f18b17a621029e6814616f5437891c26b8c5e5
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-09 17:51:04 +0000

    added roles & set cookies, fixed cors issue

--- rn-auth-backend/server.js
+++ rn-auth-backend/server.js
@@ -22,7 +22,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.set('trust proxy', 1);
 app.use(cors({
     origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002','http://localhost:3003','http://localhost:3004','http://localhost:3005','http://localhost:3006','http://localhost:3007','http://localhost:3008','http://localhost:3009','http://localhost:3010'],
     credentials: true
