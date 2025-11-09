84eecb5310dc0d4fdb54f31617f172d27ac966e5
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-09 16:58:10 +0000

    added roles & set cookies, fixed cors issue

--- rn-auth-backend/server.js
+++ rn-auth-backend/server.js
@@ -24,23 +24,9 @@ const PORT = process.env.PORT || 5000;
 
 app.set('trust proxy', 1);
 
-// Middlewares
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://auth.ekowlabs.space'
-];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        return callback(new Error('Not allowed by CORS'));
-    },
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://auth.ekowlabs.space'],
     credentials: true
 }));
 app.use(express.json());
